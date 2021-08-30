145dcc8aaedc52af43191909c7eb7de2c79f6290
Crowdbotics <team@crowdbotics.com>
2021-08-30 03:10:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "yellow_butterfly_30189Identifier",
+  url: "https://yellow_butterfly_30189Identifier.botics.co",
+  api: "https://yellow_butterfly_30189Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
